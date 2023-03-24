12ff61de691e8f73cd860eefa1f1fb1b0dd5ce8b
ed-grolsh <ed@soulppl.com>
2023-03-24 14:23:59 +0100

    fix: code style

--- projects/user_quiz/components/default/handler.py
+++ projects/user_quiz/components/default/handler.py
@@ -6,10 +6,6 @@ from utils.authorization import handled_auth_user
 
 from utils.clear_user_history import delete_message
 
-API_TOKEN = os.getenv("BOT_API_TOKEN")
-api_id = 6275419559
-api_hash = 'AAEIsSMXFIedarHGPS673SgEEFwnOEvWVe8'
-
 
 async def default(message: types.Message):
     if message.chat.type != 'private':
